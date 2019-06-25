8aa388854d61dcc8920b6ec0a3d327ad8d460b03
JunPyo <jon0397@naver.com>
2019-06-25 10:33:55 +0900

    카카오 가입기능추가 페이스 북과 버튼생성에서 충돌이 일어나기에 import부분 변경, request부분 if/else문으로 변경

--- app/src/main/java/jp/daydayflower/co/kr/returntest/LoginActivity.java
+++ app/src/main/java/jp/daydayflower/co/kr/returntest/LoginActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-
+import com.kakao.usermgmt.LoginButton;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -18,17 +18,48 @@ import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
+
 
 import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private SessionCallback callback;
     private CallbackManager callbackManager;
+    TextView user_nickname,user_email;
+    CircleImageView user_img;
+    LinearLayout success_layout;
+    Button logout_btn;
     LoginButton loginButton;
+    AQuery aQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +69,54 @@ public class LoginActivity extends AppCompatActivity {
 
         //FacebookSdk.sdkInitialize(getApplicationContext());
         //AppEventsLogger.activateApp(this);
+        aQuery = new AQuery(this);
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        loginButton = (LoginButton)findViewById(R.id.com_kakao_login);
+        com.facebook.login.widget.LoginButton loginButton_face = (com.facebook.login.widget.LoginButton) findViewById(R.id.login_button_face);
+        loginButton_face.setReadPermissions("email");
+        loginButton.setOnTouchListener(new View.OnTouchListener() {
+                                                 @Override
+                                                 public boolean onTouch(View v, MotionEvent event) {
 
-        callbackManager = CallbackManager.Factory.create();
+                                                     if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                                         if (!isConnected()) {
+                                                             Toast.makeText(LoginActivity.this, "인터넷 연결을 확인해주세요", Toast.LENGTH_SHORT).show();
+                                                         }
+                                                     }
+                                                     if (isConnected()) {
+                                                         return false;
+                                                     } else {
+                                                         return true;
+                                                     }
+                                                 }
+                                             });
 
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
+        success_layout = (LinearLayout)findViewById(R.id.success_layout);
+        user_nickname =(TextView)findViewById(R.id.user_nickname);
+        user_img =(CircleImageView) findViewById(R.id.user_img);
+        user_email =(TextView)findViewById(R.id.user_email);
+        logout_btn = (Button)findViewById(R.id.logout);
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Session.getCurrentSession().isOpened()) {
+                    requestLogout();
+                }
+            }
+        });
+
+        if(Session.getCurrentSession().isOpened()){
+            requestMe();
+        }else{
+            success_layout.setVisibility(View.GONE);
+            loginButton.setVisibility(View.VISIBLE);
+        }
+
+//여기부터 페이스북--------------------------------------------------------------------
+                callbackManager = CallbackManager.Factory.create();
+
+
         // If using in a fragment
         //loginButton.setFragment(this);
 
@@ -79,12 +153,18 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
+//Create 끝--------------------------------------------------------------------
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
         super.onActivityResult(requestCode, resultCode, data);
     }
+    //onactivityresult 관련해서 두개를 if else 문으로 하나로 합침
 
     private void getHashKey(){
         PackageInfo packageInfo = null;
@@ -106,4 +186,83 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     }
+    //위에 부분 페이스북 아랫부분 카카오톡
+
+    public boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+
+        return false;
+    }
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+            //access token을 성공적으로 발급 받아 valid access token을 가지고 있는 상태. 일반적으로 로그인 후의 다음 activity로 이동한다.
+            if(Session.getCurrentSession().isOpened()){ // 한 번더 세션을 체크해주었습니다.
+                requestMe();
+            }
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                Logger.e(exception);
+            }
+        }
+    }
+    private void requestLogout() {
+        success_layout.setVisibility(View.GONE);
+        loginButton.setVisibility(View.VISIBLE);
+        UserManagement.requestLogout(new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this, "로그아웃 성공", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void requestMe() {
+        success_layout.setVisibility(View.VISIBLE);
+        loginButton.setVisibility(View.GONE);
+
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                Log.e("onFailure", errorResult + "");
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.e("onSessionClosed",errorResult + "");
+            }
+
+            @Override
+            public void onSuccess(UserProfile userProfile) {
+                Log.e("onSuccess",userProfile.toString());
+                user_nickname.setText(userProfile.getNickname());
+                user_email.setText(userProfile.getEmail());
+                aQuery.id(user_img).image(userProfile.getThumbnailImagePath()); // <- 프로필 작은 이미지 , userProfile.getProfileImagePath() <- 큰 이미지
+            }
+
+            @Override
+            public void onNotSignedUp() {
+                Log.e("onNotSignedUp","onNotSignedUp");
+            }
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
 }
